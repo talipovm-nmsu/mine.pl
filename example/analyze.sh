1d639eec3b19f63f494a394c5c8b809d00713504
Marat R. Talipov <152807056+talipovm-nmsu@users.noreply.github.com>
2025-04-10 10:05:34 -0600

    Update analyze.sh
    simplified the output message

--- example/analyze.sh
+++ example/analyze.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
 
 
-echo 'SQ-c2 should be more stable, change the script if it is not the case'
+echo 'Checking which isomer is more stable'
 ../mine.pl --terse --td scf di-tBu-quinone/SQ-c*log
 
 echo
+
+echo 'Compute the energy of a proton transfer from 3,5-di-tBu-HQ and 1,2-Q'
 echo '--Electronic Energy--'
-echo 'Check the value, it is expected to be around or less than 10 kJ/mol to be consistent with the experiment'
 ../mine.pl --terse --kJ --td scf \
     --g1 o-quinone/SQ.log --g1 di-tBu-quinone/Q-AR.log \
     --g2 o-quinone/Q-AR.log --g2 di-tBu-quinone/SQ-c2.log
 
 echo
 echo '--Free Energy--'
-echo 'Check the value, it is expected to be around or less than 10 kJ/mol to be consistent with the experiment'
 ../mine.pl --terse --kJ --td Gibbs \
     --g1 o-quinone/SQ.log --g1 di-tBu-quinone/Q-AR.log \
     --g2 o-quinone/Q-AR.log --g2 di-tBu-quinone/SQ-c2.log
